57508f29cf4e60c7af0435dccdf5f208fc9c7fd3
israel filho <israel.filho@aluno.uepb.edu.br>
2025-05-07 18:25:57 -0300

    feat:  mundando header

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -5,7 +5,7 @@ import './header.css'
 function Header() {
     return(
         <header>
-            <Link className="logo" to="/">Prime Flix</Link>
+            <Link className="logo" to="/">Prime<span>View</span></Link>
             <Link className="favoritos" to="/favoritos">Meus filmes</Link>
         </header>
     )
