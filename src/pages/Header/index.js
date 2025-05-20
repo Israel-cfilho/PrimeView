286a360e7f60b48b47b1fb90fae3104ff3ddf0d3
israel filho <israel.filho@aluno.uepb.edu.br>
2025-05-20 18:08:43 -0300

    feat: add icon

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -6,7 +6,10 @@ function Header() {
     return(
         <header>
             <Link className="logo" to="/">Prime<span>View</span></Link>
+            <div className="header">
             <Link className="favoritos" to="/favoritos">Meus filmes</Link>
+            {/* <Link><ion-icon name="person-circle-outline"></ion-icon></Link> */}
+            </div>
         </header>
     )
 }
