74982484f037b49b2868c16fbe5d2354c8d0bf17
israel filho <israel.filho@aluno.uepb.edu.br>
2025-05-25 15:00:52 -0300

    feat: add bootstrap

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,14 @@
-import './home.css'
+import './home.css';
 import { useEffect, useState } from "react";
 import api from '../../services/api';
-import { Link } from "react-router-dom";
-import { Spinner } from 'react-bootstrap';
-import { ListaFilmes, FilmeCard, FilmeImagem, BotaoAcessar } from '../Home/movieStyled'
+import { Spinner, Container } from 'react-bootstrap'; 
+import { ListaFilmes, FilmeCard, FilmeImagem, BotaoAcessar } from '../Home/movieStyled';
 import Busca from '../Search';
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [termoBusca, setTermoBusca] = useState("");
-
 
   useEffect(() => {
     async function loadFilmes() {
@@ -24,7 +22,6 @@ function Home() {
         });
         setFilmes(response.data.results.slice(0, 20));
         setLoading(false);
-        
       } catch (error) {
         console.error("Erro ao carregar filmes:", error);
       }
@@ -33,36 +30,33 @@ function Home() {
     loadFilmes();
   }, []);
 
-
-
   if (loading) {
     return (
       <div>
-        <div class="text-center">
+        <div className="text-center">
           <Spinner>
-            <span class="visually-hidden">Carregando...</span>
+            <span className="visually-hidden">Carregando...</span>
           </Spinner>
         </div>
       </div>
-    )
+    );
   }
 
   return (
-
-    <div className="container">
+    <Container className="py-4">
       <h1 className='titulo-lista'>
-  {termoBusca ? `Resultados de: "${termoBusca}"` : "Filmes populares"}
-</h1>
+        {termoBusca ? `Resultados de: "${termoBusca}"` : "Filmes populares"}
+      </h1>
       <Busca setFilmes={setFilmes} setLoading={setLoading} setTermoBusca={setTermoBusca} />
       <ListaFilmes>
         {filmes.map((filme) => (
           <FilmeCard key={filme.id}>
-            <FilmeImagem  src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} />
+            <FilmeImagem src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} />
             <BotaoAcessar to={`/filme/${filme.id}`}>Acessar</BotaoAcessar>
           </FilmeCard>
         ))}
       </ListaFilmes>
-    </div>
+    </Container>
   );
 }
 
